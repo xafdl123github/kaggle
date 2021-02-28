8ab9ee19c2fc89f0ef805d12edde774f1c3fc70c
fulu <946902732@qq.com>
2021-02-28 23:07:22 +0800

    0228 2307

---
+++ kaggle/华为机考/1.py
@@ -0,0 +1,5 @@
+import re
+from icecream import ic
+
+c = 'e'
+ic(ord(c), chr(ord(c) + 1))
---
+++ kaggle/华为机考/test.py
@@ -0,0 +1,64 @@
+from icecream import ic
+import re
+import sys
+
+while 1:
+    try:
+        N = int(input())  # 同学数量
+
+        sec_row = input()
+        stu_lst = sec_row.split()  # 同学列表
+
+        if N != len(stu_lst):
+            continue
+
+        res_lst = []
+        for idx in range(1, N-1):   # 循环每一个元素
+            # left
+            left_con = []
+            for left_idx in range(idx):
+
+                if len(left_con) == 0:
+                    if stu_lst[left_idx] < stu_lst[idx]:
+                        left_con.append(stu_lst[left_idx])
+                else:
+                    if stu_lst[left_idx] <= left_con[-1]:
+                        left_con = []
+                        left_con.append(stu_lst[left_idx])
+                    else:  # 当前元素比容器（递增序列）最后一个元素大
+                        if stu_lst[left_idx] < stu_lst[idx]:
+                            left_con.append(stu_lst[left_idx])
+                        else:
+                            left_con = []
+
+            ic(idx, left_con)
+
+            # right
+            right_con = []
+            right_seq = stu_lst[idx + 1:]
+            right_seq = right_seq[::-1]   # 当前元素的右边序列（不包含当前元素）
+            # ic(right_seq)
+            for right_idx in range(len(right_seq)):
+                if len(right_con) == 0:
+                    if right_seq[right_idx] < stu_lst[idx]:
+                        # ic(idx, right_seq[right_idx])
+                        right_con.append(right_seq[right_idx])
+                else:
+                    if right_seq[right_idx] <= right_con[-1]:
+                        right_con = []
+                        right_con.append(right_seq[right_idx])
+                    else:  # 当前元素比容器（递增序列）最后一个元素大
+                        if right_seq[right_idx] < stu_lst[idx]:
+                            right_con.append(right_seq[right_idx])
+                        else:
+                            right_con = []
+
+            ic(idx, right_con)
+
+            if len(left_con) > 0 and len(right_con) > 0:
+                res_lst.append(len(left_con) + len(right_con) + 1)
+
+        print(N - max(res_lst))
+
+    except:
+        break
