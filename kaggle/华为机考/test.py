a7ca56676a5d06c19a85ff38184c782116383169
fulu <946902732@qq.com>
2021-03-02 07:50:22 +0800

    a

--- kaggle/华为机考/test.py
+++ kaggle/华为机考/test.py
@@ -1,39 +1,14 @@
-def issu(x):
-    tem = 2
-    while tem ** 2 <= x:
-        if x % tem == 0:
-            return False
-        tem += 1
-    return True
+import re
 
+while True:
+    try:
+        seq = input()
 
-def find(ji_number, l1, l2, ou):
-    for i in range(0, len(ou)):
-        if issu(ji_number + ou[i]) and l1[i] == 0:
-            l1[i] = 1
-            if l2[i] == 0 or find(l2[i], l1, l2, ou):
-                l2[i] = ji_number
-                return True
-    return False
+        res = re.findall(r'[a-zA-Z]+', seq)
+        if not res:
+            break
 
-
-try:
-    while True:
-        n = input()
-        n = int(n)
-        l = list(map(int, input().split()))
-        ji, ou = [], []
-        for i in range(n):
-            if l[i] % 2 == 0:
-                ou.append(l[i])
-            else:
-                ji.append(l[i])
-        result = 0
-        match = [0] * len(ou)
-        for i in range(0, len(ji)):
-            used = [0] * len(ou)
-            if find(ji[i], used, match, ou):
-                result += 1
-        print(result)
-except:
-    pass
+        lst = res[::-1]
+        print(' '.join(lst))
+    except:
+        break
