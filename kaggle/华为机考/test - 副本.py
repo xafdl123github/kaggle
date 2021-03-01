775566816eaa3c1f480dcc88cf199358d10d2a55
fulu <946902732@qq.com>
2021-03-01 22:23:56 +0800

    0301 2200

--- kaggle/华为机考/test - 副本.py
+++ kaggle/华为机考/test - 副本.py
@@ -1,61 +1,101 @@
-import re
-import sys
+from icecream import ic
 
-while 1:
-    try:
-        N = int(input())  # 同学数量
+# 判断偶数
+def judge_oushu(digit):
+    if digit % 2 == 0:
+        return True
+    else:
+        return False
 
-        sec_row = input()
-        stu_lst = sec_row.split()  # 同学列表
+# 判断奇数
+def judge_qishu(digit):
+    if digit % 2 != 0:
+        return True
+    else:
+        return False
 
-        if N != len(stu_lst):
-            continue
+# 判断素数
+def judge_prime(digit):
+    if digit % 2 == 0:  # 偶数
+        return False
+    else:   # 奇数
+        for i in range(2, digit//2):
+            if digit % i == 0:
+                return False
+        return True
 
-        res_lst = []
-        for idx in range(1, N-1):   # 循环每一个元素
-            # left   186 186 150 200 160 130 197 200
-            left_con = []
-            for left_idx in range(idx):   # 循环当前元素左侧每一个元素
 
-                if len(left_con) == 0:
-                    if stu_lst[left_idx] < stu_lst[idx]:
-                        left_con.append(stu_lst[left_idx])
-                else:
-                    if stu_lst[left_idx] <= left_con[-1]:
-                        left_con = []
-                        left_con.append(stu_lst[left_idx])
-                    else:  # 当前元素比容器（递增序列）最后一个元素大
-                        if stu_lst[left_idx] < stu_lst[idx]:
-                            left_con.append(stu_lst[left_idx])
-                        else:
-                            left_con = []
+def find_prime(even_lst, odd_lst):
+    lines = []
+    for r in range(len(even_lst)):  # 行
+        row_primes = []
+        for c in odd_lst:  # 列
+            if judge_prime(r + c):
+                row_primes.append(c)
 
-            # right  186 186 150 200 160 130 197 200
-            right_con = []
-            right_seq = stu_lst[idx + 1:]
-            right_seq = right_seq[::-1]   # 当前元素的右边序列（不包含当前元素）
-            # ic(right_seq)
-            right_xiao_cur = []
-            for right_idx in range(len(right_seq)):     # 循环当前元素右侧每一个元素
+        lines.append(len(row_primes))
 
-                if len(right_con) == 0:
-                    if right_seq[right_idx] < stu_lst[idx]:
-                        # ic(idx, right_seq[right_idx])
-                        right_con.append(right_seq[right_idx])
-                else:
-                    if right_seq[right_idx] <= right_con[-1]:
-                        right_con = []
-                        right_con.append(right_seq[right_idx])
-                    else:  # 当前元素比容器（递增序列）最后一个元素大
-                        if right_seq[right_idx] < stu_lst[idx]:
-                            right_con.append(right_seq[right_idx])
-                        else:
-                            right_con = []
+    # ic(lines)
 
-            if len(left_con) > 0 and len(right_con) > 0:
-                res_lst.append(len(left_con) + len(right_con) + 1)
+    if not lines:
+        return 0
 
-        print(N - max(res_lst))
+    if max(lines) == 0:
+        return 0
 
-    except:
-        break
+    lines = list(filter(lambda x:x!=0, lines))
+
+    # 在列表中找到最小数的索引
+    r_index = lines.index(min(lines))  # 如果存在相同的数，则取第一次出现的索引
+    min_row = even_lst[r_index]  # 最小偶数
+
+    del even_lst[r_index]
+
+    for key, c in enumerate(odd_lst):
+        if judge_prime(min_row + c):  # 如果是素数
+            del odd_lst[key]
+            return 1 + find_prime(even_lst, odd_lst)
+
+    return 0
+
+
+while True:
+    # try:
+    row1 = input()
+    num = int(row1) # 数据个数
+
+    row2 = input()
+    num_lst = row2.split()
+    num_lst = list(map(int, num_lst))   # 转为整形
+
+    # 偶数和奇数的个数不一定相等
+    evens = list(filter(judge_oushu, num_lst))   # 偶数列表   行
+    odds = list(filter(judge_qishu, num_lst))    # 奇数列表   列
+
+    prime_pair_num = find_prime(evens, odds)
+
+    # ic(evens, odds)
+
+    # if len(evens) == len(odds):
+    #     prime_pair_num = find_prime(evens, odds)
+    # elif len(evens) > len(odds):
+    #     prime_pair_num = find_prime(evens[:len(odds)], odds)
+    #     yushu = evens[len(odds):]
+    #     new_evens = yushu[:int(len(yushu)/2)]
+    #     new_odds = yushu[int(len(yushu)/2):]
+    #     prime_pair_num += find_prime(new_evens, new_odds)
+    # else:
+    #     prime_pair_num = find_prime(evens, odds[:len(evens)])
+    #     ic(prime_pair_num)
+    #     yushu = odds[len(evens):]
+    #     ic(len(yushu), len(yushu) / 2)
+    #     new_evens = yushu[:int(len(yushu) / 2)]
+    #     new_odds = yushu[int(len(yushu) / 2):]
+    #     prime_pair_num += find_prime(new_evens, new_odds)
+    #     ic(prime_pair_num)
+
+    print(prime_pair_num)
+
+
+    # except:
+    #     break
--- kaggle/华为机考/test.py
+++ kaggle/华为机考/test.py
@@ -1,59 +1,39 @@
-from icecream import ic
+def issu(x):
+    tem = 2
+    while tem ** 2 <= x:
+        if x % tem == 0:
+            return False
+        tem += 1
+    return True
 
 
-# 判断偶数
-def judge_oushu(digit):
-    if digit % 2 == 0:
-        return True
-    else:
-        return False
-
-# 判断奇数
-def judge_qishu(digit):
-    if digit % 2 != 0:
-        return True
-    else:
-        return False
-
-# 判断素数
-def judge_prime(digit):
-    if digit % 2 == 0:  # 偶数
-        return False
-    else:   # 奇数
-        for i in range(2, digit//2):
-            if digit % i == 0:
-                return False
-        return True
-
-while True:
-    try:
-        row1 = input()
-        num = int(row1) # 数据个数
-
-        row2 = input()
-        num_lst = row2.split()
-        num_lst = list(map(int, num_lst))   # 转为整形
-
-        # 偶数和奇数的个数不一定相等
-        oushu_lst = filter(judge_oushu, num_lst)   # 偶数列表   行
-        qishu_lst = filter(judge_qishu, num_lst)    # 奇数列表   列
-
-        lines = []
-        for r in range(oushu_lst):   # 行
-            row_primes = []
-            for c in qishu_lst:    # 列
-                if judge_prime(r + c):
-                    row_primes.append(c)
-
-            lines.append(len(row_primes))
-
-        # 在列表中找到最小数的索引
-        r_index = lines.index(min(lines))   # 如果存在相同的数，则取第一次出现的索引
+def find(ji_number, l1, l2, ou):
+    for i in range(0, len(ou)):
+        if issu(ji_number + ou[i]) and l1[i] == 0:
+            l1[i] = 1
+            if l2[i] == 0 or find(l2[i], l1, l2, ou):
+                l2[i] = ji_number
+                return True
+    return False
 
 
-
-
-
-
-    except:
-        break
+try:
+    while True:
+        n = input()
+        n = int(n)
+        l = list(map(int, input().split()))
+        ji, ou = [], []
+        for i in range(n):
+            if l[i] % 2 == 0:
+                ou.append(l[i])
+            else:
+                ji.append(l[i])
+        result = 0
+        match = [0] * len(ou)
+        for i in range(0, len(ji)):
+            used = [0] * len(ou)
+            if find(ji[i], used, match, ou):
+                result += 1
+        print(result)
+except:
+    pass
