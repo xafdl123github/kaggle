ad02ffd45d2b60425e48f1813481d3004992ad9b
fulu <946902732@qq.com>
2021-03-02 17:55:57 +0800

    d

--- kaggle/华为机考/1.py
+++ kaggle/华为机考/1.py
@@ -1,5 +1,75 @@
-import re
-from icecream import ic
+# 检查ip是否合法
+def check_ip(ip):  # ip=['192','168','0','1']
+    if len(ip) != 4 or '' in ip:
+        return False
+    else:
+        for c in ip:
+            if int(c) < 0 or int(c) > 255:
+                return False
+        return True
 
-c = 'e'
-ic(ord(c), chr(ord(c) + 1))
+# 检查ms是否合法(数值大小必须满足前面是连续的1，后面是连续的0，全1或者全0都非法)
+lll = ['254','252','248','240','224','192','128','0']
+
+def check_ms(ms):  # ms=['255','255','255','0']
+    if len(ms) != 4 or '' in ms:
+        return False
+    if ms[0] == '255':
+        if ms[1] == '255':
+            if ms[2] == '255':
+                if ms[3] in lll:
+                    return True
+                else:
+                    return False
+            elif ms[2] in lll and ms[3] == '0':
+                return True
+            else:
+                return False
+        elif ms[1] in lll and ms[2] == '0' and ms[3] == '0':
+            return True
+        else:
+            return False
+    elif ms[0] in lll[:-1] and ms[1] == '0' and ms[2] == '0' and ms[3] == '0':
+        return True
+    else:
+        return False
+
+def trans_to_bin(ip_or_ms):  # ip_or_ms=['192','168','0','1']
+    bin_str = ''
+    for field in ip_or_ms:
+        bin_str += format(int(field), 'b').zfill(8)
+    return bin_str
+
+def ip_and_ms(ip_bin, ms_bin):
+    and_str = ''  # ip二进制串与ms二进制串相与后的二进制串
+    for i in range(32):
+        if ip_bin[i] == '1' and ms_bin[i] == '1':
+            and_str += '1'
+        else:
+            and_str += '0'
+    return and_str
+
+# 主函数部分
+while True:
+    try:
+        ms = input().strip().split('.')  # ms=['255','255','255','0']
+        ip1 = input().strip().split('.')  # ip1=['192','168','0','1']
+        ip2 = input().strip().split('.')
+
+        # 若ip1,ip2和ms都合法，才进一步判断ip1与ip2是否在同一个子网
+        if check_ms(ms) and check_ip(ip1) and check_ip(ip2):
+            ms_bin = trans_to_bin(ms)
+            ip1_bin = trans_to_bin(ip1)
+            ip2_bin = trans_to_bin(ip2)
+
+            str1 = ip_and_ms(ip1_bin, ms_bin)
+            str2 = ip_and_ms(ip2_bin, ms_bin)
+
+            if str1 == str2:
+                print(0)
+            else:
+                print(2)
+        else:
+            print(1)
+    except:
+        break
--- kaggle/华为机考/test.py
+++ kaggle/华为机考/test.py
@@ -1,59 +1,14 @@
-from icecream import ic
-
-
-# 判断偶数
-def judge_oushu(digit):
-    if digit % 2 == 0:
-        return True
+def fun(n):
+    if n <= 2:
+        return 1
+    elif n == 3:
+        return 2
     else:
-        return False
-
-# 判断奇数
-def judge_qishu(digit):
-    if digit % 2 != 0:
-        return True
-    else:
-        return False
-
-# 判断素数
-def judge_prime(digit):
-    if digit % 2 == 0:  # 偶数
-        return False
-    else:   # 奇数
-        for i in range(2, digit//2):
-            if digit % i == 0:
-                return False
-        return True
+        return fun(n-1) + fun(n-2)
 
 while True:
     try:
-        row1 = input()
-        num = int(row1) # 数据个数
-
-        row2 = input()
-        num_lst = row2.split()
-        num_lst = list(map(int, num_lst))   # 转为整形
-
-        # 偶数和奇数的个数不一定相等
-        oushu_lst = filter(judge_oushu, num_lst)   # 偶数列表   行
-        qishu_lst = filter(judge_qishu, num_lst)    # 奇数列表   列
-
-        lines = []
-        for r in range(oushu_lst):   # 行
-            row_primes = []
-            for c in qishu_lst:    # 列
-                if judge_prime(r + c):
-                    row_primes.append(c)
-
-            lines.append(len(row_primes))
-
-        # 在列表中找到最小数的索引
-        r_index = lines.index(min(lines))   # 如果存在相同的数，则取第一次出现的索引
-
-
-
-
-
-
+        n = int(input())
+        print(fun(n))
     except:
         break
